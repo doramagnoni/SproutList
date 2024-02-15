5dadf62f8d2da1151ebb92d19ddc9491fe5177b6
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-02-15 12:05:22 +0100

    Added initial plant model, database table, and basic list view

--- sproutlist/plants/models.py
+++ sproutlist/plants/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    watering_interval = models.IntegerField(help_text="Days between watering")
+    last_watered = models.DateField(auto_now_add=True)  # Auto-add on creation
+    fertilization_interval = models.IntegerField(help_text="Days between fertilization")
+    last_fertilized = models.DateField(auto_now_add=True)
+
+class ToDo(models.Model):
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    task_type = models.CharField(max_length=20, choices=[('WATER', 'Water'), ('FERTILIZE', 'Fertilize')])
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
--- sproutlist/plants/views.py
+++ sproutlist/plants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import Plant, ToDo
+
+def plant_list(request):
+    plants = Plant.objects.all()
+    todos = ToDo.objects.filter(completed=False)
+    return render(request, 'plants/plant_list.html', {'plants': plants, 'todos': todos})
